54b79a6d2f3c157e13b633680ecd4c1b9d11a845
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2022-04-25 13:17:40 -0400

    installed styled components/react-splide, render images to page

--- my-app/src/components/Popular.jsx
+++ my-app/src/components/Popular.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import styled from 'styled-components';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
 
 function Popular() {
   const [popular, setPopular] = useState([]);
@@ -19,13 +22,39 @@ function Popular() {
     <div>
       {popular.map((recipe) => {
         return (
-          <div>
-            <p>{recipe.title}</p>
-          </div>
+          <Wrapper>
+            <h3>Popular Picks</h3>
+            {popular.map((recipe) => {
+              return (
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </Card>
+              )
+            })}
+          </Wrapper>
         );
       })}
     </div>
   );
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+
+  img {
+    border-radius: 2rem;
+  }
+
+  div img {
+
+  }
+`;
 
 export default Popular;
